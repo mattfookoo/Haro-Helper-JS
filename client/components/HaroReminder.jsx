cb6259aba2d33414695c6726281567f665af1432
mattfookoo <matthewfukudome@gmail.com>
2023-03-02 17:05:52 -0800

    added some more styling

--- client/components/HaroReminder.jsx
+++ client/components/HaroReminder.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styling/KitCards.css';
+import '../styling/HaroReminder.css';
 
 const HaroReminder = () => {
   const [kits, setKits] = useState([]);
@@ -32,13 +32,18 @@ const HaroReminder = () => {
     return days;
   };
 
+  if (oldKits.length === 0) {
+    return null;
+  }
+
   return (
-    <div>
-        <p>Danger! Danger! Backlog has old entries! The following kits are over 30 days old, Haro!</p>
+    <div className="pop-up">
+      <p><p style={{ color: 'red' }}>Danger! Danger!</p>Backlog has old entries! The following kits are over 30 days old, Haro!</p>
       {oldKits.map((kit) => (
         <li key={kit._id}>{kit.kit} was added {calculateDays(kit.createdAt)} days ago!!!</li> 
       ))}
       <p>Please consider constructing your old kits before buying new ones, Haro!</p>
+      <p>Your wallet levels are already critical! Critical!</p>
     </div>
   );
 };
